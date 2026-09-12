549d15b6e8f014b3c71e473726424231d9324efb
Dxvid-py <emanueldavxd@gmail.com>
2026-09-12 12:53:54 -0500

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -84,7 +84,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { title: "Floristería Deluxe Premium · Atelier floral en Barranquilla" },
+      { title: "Floristería Deluxury · Atelier floral en Barranquilla" },
       {
         name: "description",
         content:
